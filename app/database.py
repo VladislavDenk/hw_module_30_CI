23221227e0def20b3aeef1dd0cf753948edc3e97
vladislav_ivanov_7 <ivanovvladislav070@gmail.com>
2026-01-15 05:39:27 +0700

    Correction database and linter files

--- app/database.py
+++ app/database.py
@@ -1,14 +1,11 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, \
-    create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session = async_sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 session = async_session()
 Base = declarative_base()
